bf30c36bed5eb394eec509c412074e0c3458481e
Lucas Cupertino <olucas.cdev@gmail.com>
2023-06-17 15:22:35 -0300

    atualização e finalização

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectToDatabase = async () => {
-    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nodelearn.mvxptr0.mongodb.net/database?retryWrites=true&w=majority`).then(console.log("Conexão efetuada com sucesso!")).catch(error => {
+    await mongoose
+    .connect(`mongodb+srv://lvcxs:${process.env.MONGODB_PASSWORD}@nodelearn.mvxptr0.mongodb.net/`)
+    .then(console.log("Conexão efetuada com sucesso!"))
+    .catch(error => {
         console.log("Ocorreu um erro ao realizar a conexão! Erro: ", error)
     })
 }
